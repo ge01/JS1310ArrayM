0e5c308b34fb78777a61917c3686497076f666de
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-26 18:56:16 -0500

    popping and pushing

--- app.js
+++ app.js
@@ -5,3 +5,33 @@ var fruits = ["Banana", "Orange", "Apple", "Mango"];
 document.getElementById("to-string").innerHTML = fruits.toString();
 
 document.getElementById("fruits").innerHTML = fruits;
+
+/****************************************************
+*                 The join() method                 *
+****************************************************/
+document.getElementById("join").innerHTML =
+fruits.join(" * ");
+
+document.getElementById("join1").innerHTML =
+fruits.join(" - ");
+
+/****************************************************
+*                Popping and Pushing                *
+****************************************************/
+document.getElementById("pop-function").innerHTML = fruits;
+
+function popFunction() {
+  fruits.pop();
+  document.getElementById("pop-function").innerHTML = fruits;
+}
+
+document.getElementById("pop-function1").innerHTML = fruits;
+
+function popFunction1() {
+  document.getElementById("pop-function1").innerHTML = fruits.pop();
+}
+
+function pushFunction() {
+  fruits.push("Kiwi");
+  document.getElementById("push-function").innerHTML = fruits;
+}

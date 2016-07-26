a3ccf59bef5a515f03b517f40e84c0409143dcdf
Gabe Escobedo <escobedo.gabe@yahoo.com>
2016-07-26 11:14:29 -0500

    converting arrays to strings

---
+++ app.js
@@ -0,0 +1,7 @@
+/****************************************************
+*            Converting Arrays to Strings           *
+****************************************************/
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("to-string").innerHTML = fruits.toString();
+
+document.getElementById("fruits").innerHTML = fruits;
